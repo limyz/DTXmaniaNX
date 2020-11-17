3540099a40f653616d471159e1947f04767fb17d
marika <marika@waifu.club>
2020-11-17 06:38:01 -0400

    Allow forcing classic level display.

--- DTXMania/Code/Stage/05.SongSelection/CActSelectStatusPanel.cs
+++ DTXMania/Code/Stage/05.SongSelection/CActSelectStatusPanel.cs
@@ -316,8 +316,15 @@ namespace DTXMania
                                 {
                                     // convert the level back into a whole number (0-999) to make it easier to work with
                                     int nLevel = (n現在選択中の曲のレベル難易度毎DGB[i][j] * 10) + n現在選択中の曲のレベル小数点難易度毎DGB[i][j];
-                                    bool bShowClassicLevel = CDTXMania.ConfigIni.nSkillMode == 0;
                                     bool bHasSong = b現在選択中の曲に譜面がある[i][j];
+                                    bool bShowClassicLevel = CDTXMania.ConfigIni.nSkillMode == 0 ||
+                                                             (CDTXMania.ConfigIni.bCLASSIC譜面判別を有効にする &&
+                                                             !CDTXMania.DTX.bチップがある.LeftCymbal &&
+                                                             !CDTXMania.DTX.bチップがある.LP &&
+                                                             !CDTXMania.DTX.bチップがある.LBD &&
+                                                             !CDTXMania.DTX.bチップがある.FT &&
+                                                             !CDTXMania.DTX.bチップがある.Ride &&
+                                                             !CDTXMania.DTX.b強制的にXG譜面にする);
 
                                     int nX = nBoxX + nPanelW - 77;
                                     int nY = nBoxY + nPanelH - 35;
