5fe5584d549615a1116a9494f3015097fb90ea5d
marika <marika@waifu.club>
2020-12-02 15:16:34 -0400

    Allow limiting the range of CIntSetting values.

--- DTXMania/Code/Configuration/CIntSetting.cs
+++ DTXMania/Code/Configuration/CIntSetting.cs
@@ -5,15 +5,42 @@
     /// </summary>
     public class CIntSetting : ISetting<int>
     {
+        /// <summary>
+        /// The inclusive minimum value that this setting's value can be, or <see cref="null"/> if their is no lower limit.
+        /// </summary>
+        private readonly int? nMinValue;
+
+        /// <summary>
+        /// The inclusive maximum value that this setting's value can be, or <see cref="null"/> if their is no upper limit.
+        /// </summary>
+        private readonly int? nMaxValue;
+
         public string strKey { get; }
 
-        public CIntSetting(string strKey)
+        public CIntSetting(string strKey, int? nMinValue = null, int? nMaxValue = null)
         {
             this.strKey = strKey;
+            this.nMinValue = nMinValue;
+            this.nMaxValue = nMaxValue;
         }
 
-        public string tEncode(int value) => value.ToString();
+        public string tEncode(int value) => tLimitValueRange(value).ToString();
 
-        public int tDecode(string strValue) => int.Parse(strValue);
+        public int tDecode(string strValue) => tLimitValueRange(int.Parse(strValue));
+
+        /// <summary>
+        /// Limit the given value to this setting's range.
+        /// </summary>
+        /// <param name="nValue">The <see cref="int"/> to limit.</param>
+        private int tLimitValueRange(int nValue)
+        {
+            if (nMinValue is int nMin && nValue < nMin)
+                return nMin;
+
+            if (nMaxValue is int nMax && nValue > nMax)
+                return nMax;
+
+            return nValue;
+        }
     }
 }
