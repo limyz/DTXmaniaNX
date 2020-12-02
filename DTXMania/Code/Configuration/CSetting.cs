a893859a6ae4e24b56c87f6872508fe184e46186
marika <marika@waifu.club>
2020-12-02 16:22:47 -0400

    Add an empty CSetting class.
    This is the root scope for ISetting<T> constants.

---
+++ DTXMania/Code/Configuration/CSetting.cs
@@ -0,0 +1,17 @@
+﻿using DTXMania.Configuration.Settings;
+
+namespace DTXMania.Configuration
+{
+    /// <summary>
+    /// All the available <see cref="ISetting{T}"/>s to use within a <see cref="CConfigurationStore"/>.
+    /// </summary>
+    /// <remarks>
+    /// This is intended to function as a pseudo-enumerated type, existing purely as a scope for settings.
+    /// </remarks>
+    public sealed class CSetting
+    {
+        private CSetting()
+        {
+        }
+    }
+}
